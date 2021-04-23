b5e3c08d7e06218d7bf301db1564b02068a5dea7
Nikolay Aleksandrov <aivonix@gmail.com>
2021-04-23 16:09:49 +0300

    returned the /boxes url after the move to a fresh codebase

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/boxes', [App\Http\Controllers\BoxesController::class, 'index'])->name('boxes');
